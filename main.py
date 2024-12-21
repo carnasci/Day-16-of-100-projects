758b05c5c0a697e2a599c904dd3bf6e427b77c73
carnasci <cen.master24@gmail.com>
2024-12-21 12:26:04 -0600

    Day 16 - quiz finished

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("black", "darkgreen")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("black", "darkgreen")
+from prettytable import PrettyTable
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table = PrettyTable()
+table.add_column("Pokemon", ["Pikachu", "Charmander", "Squirtle"])
+table.add_column("Type", ["Electric", "Fire", "Water"])
+table.align = "l"
+
+print(table)
